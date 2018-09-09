2add43d2e8cea8cc77130711f44b439e73a069cf
Loay <Loay@DESKTOP-7852KET.home>
2018-09-09 22:16:06 +0300

    added swagger

--- Boostani/backend-api/src/main/java/com/boostani/backend/api/config/SecurityConfig.java
+++ Boostani/backend-api/src/main/java/com/boostani/backend/api/config/SecurityConfig.java
@@ -8,14 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
@@ -55,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/login*", "/signin/**", "/signup/**", "/campain/**", "/account/**", "/affilate/**", "/category/**", "/fund/**").permitAll()
+		http.csrf().disable().authorizeRequests().antMatchers("/**", "/login*", "/signin/**", "/signup/**", "/campain/**", "/account/**", "/affilate/**", "/category/**", "/fund/**").permitAll()
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout();
 	}
 
@@ -66,5 +61,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new ProviderSignInController(connectionFactoryLocator, usersConnectionRepository,
 				new FacebookSignInAdapter());
 	}
-
 }
